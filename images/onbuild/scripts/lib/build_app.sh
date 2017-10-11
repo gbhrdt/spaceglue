4924c8c07e089f6e2eda985f66163c3c1003d6d4
Marius Gebhardt <marius.gebhardt@gmail.com>
2017-10-11 02:22:45 +0200

    added git to builddeps, improve yarn install command

--- images/onbuild/scripts/lib/build_app.sh
+++ images/onbuild/scripts/lib/build_app.sh
@@ -94,10 +94,11 @@ meteor_version_app=$(cat .meteor/release)
 echo "  > ${meteor_version_app}"
 
 echo "=> Installing meteor yarn"
+$meteor_bin npm config set registry http://registry.npmjs.org/
 $meteor_bin npm install -g yarn -dd
 
-echo "=> Executing yarn install --production"
-$meteor_bin yarn install --production --verbose
+echo "=> Executing yarn install"
+$meteor_bin yarn install --production --ignore-engines
 
 echo "=> Executing Meteor Build..."
 
